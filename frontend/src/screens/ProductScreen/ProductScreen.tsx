ba712ce138d1ad57fbe6b8ef28d302903c0dbf29
Andrey Dmitriev <andrydmit@gmail.com>
2020-12-11 23:59:08 +0200

    working on ProductScreen

--- frontend/src/screens/ProductScreen/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen/ProductScreen.tsx
@@ -1,10 +1,65 @@
 import React from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { Col, Row, Image, ListGroup, Card } from 'react-bootstrap';
+import products from 'data/products';
+import { Props } from './types';
+import Rating from 'components/Rating';
 
-const ProductScreen = () => {
-  return (
+const ProductScreen: React.FC<Props> = () => {
+  const { id } = useParams() as { id: string };
+  const product = products.find((p) => p._id === id);
+
+  return product ? (
     <>
-      <h1>Product Details</h1>
+      <Link className="btn btn-dark my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Price: ${product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </>
+  ) : (
+    <div>No product found</div>
   );
 };
 
